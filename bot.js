69bfa3efd2f0051457d7cebe93d2decb97fe2d08
INT31302 <tkdwo287@gmail.com>
2020-01-11 22:26:21 +0900

    add dateCheck Function

--- bot.js
+++ bot.js
@@ -11,6 +11,11 @@ Date.prototype.yyyymmdd = function() {
   );
 };
 
+function createGroup(room, sender) {
+  DataBase.appendDataBase(room, root + sender);
+  return true;
+}
+
 function removeGroup(room) {
   DataBase.removeDataBase(room);
 }
@@ -28,6 +33,26 @@ function addPeople(room, msg) {
   } else {
     return false;
   }
+}
+
+function createList(room, msg, count, str) {
+  temp = ".명단작성";
+  let word = msg.substring(temp.length + 1);
+  let temp_word = word.substring(0, word.indexOf(","));
+  word = word.substring(word.indexOf(",") + 2);
+  for (i = 0; i < count(room); i++) {
+    str = str.replace("인원 " + i + " : ", temp_word);
+    if (temp_word != "null") {
+      temp_word = word.substring(0, word.indexOf(","));
+      if (word.indexOf(",") === -1) {
+        temp_word = word;
+      } else {
+        word = word.substring(word.indexOf(",") + 2);
+      }
+    }
+  }
+  DataBase.setDataBase(room, str);
+  return true;
 }
 
 function checkCommit(date, room, Doc, str, count) {
@@ -50,29 +75,60 @@ function checkCommit(date, room, Doc, str, count) {
   }
   return result;
 }
-function createGroup(room, sender) {
-  DataBase.appendDataBase(room, root + sender);
-  return true;
-}
 
-function createList(room, msg, count, str) {
-  temp = ".명단작성";
-  let word = msg.substring(temp.length + 1);
-  let temp_word = word.substring(0, word.indexOf(","));
-  word = word.substring(word.indexOf(",") + 2);
-  for (i = 0; i < count(room); i++) {
-    str = str.replace("인원 " + i + " : ", temp_word);
-    if (temp_word != "null") {
-      temp_word = word.substring(0, word.indexOf(","));
-      if (word.indexOf(",") === -1) {
-        temp_word = word;
+function checkDate(msg, room) {
+  let month = Number(msg.split(" ").reverse()[0]);
+  let results = null;
+  if (!isNaN(month)) {
+    if (month >= 1 && month <= 12) {
+      if (month < 10) {
+        month = "0" + month.toString();
       } else {
-        word = word.substring(word.indexOf(",") + 2);
+        month = month.toString();
       }
+    } else {
+      return (results = "0");
     }
+    const data = DataBase.getDataBase(room);
+    if (data.indexOf(msg.split(" ")[1]) != -1) {
+      const context = data
+        .substring(data.indexOf(msg.split(" ")[1]))
+        .split("\n")[0];
+      const name = context.split(":")[0].trim();
+      const id = context.split(":")[1].trim();
+      let doc = Utils.parse("https://ghchart.rshah.org/" + id).select(
+        "rect[data-date*=2019-" + month + "]"
+      );
+      let element = doc.toString();
+      let checkcount = element.match(/>/g).length;
+      var myArray = [];
+      var datecnt = 0;
+      for (let i = 0; i < checkcount; i++) {
+        let temp = element.substring(0, element.indexOf(">") + 1);
+        element = element.substring(element.indexOf(">") + 1).trim();
+        let date = temp.split(" ")[3];
+        let cnt = temp.split(" ")[2];
+        if (cnt === 'data-score="0"') {
+          myArray[datecnt] = date.substring(11, 21);
+          datecnt++;
+        }
+      }
+      myArray.sort(function(a, b) {
+        let dateA = a.split("-")[2];
+        let dateB = b.split("-")[2];
+        return dateA > dateB ? 1 : -1;
+      });
+      results = name + "(" + id + ")님이\n커밋 안한 날짜\n총 횟수 : " + datecnt;
+      for (let i = 0; i < datecnt; i++) {
+        results += "\n" + myArray[i];
+      }
+      return results;
+    } else {
+      return (results = "1");
+    }
+  } else {
+    return (results = "0");
   }
-  DataBase.setDataBase(room, str);
-  return true;
 }
 
 function response(
@@ -131,37 +187,17 @@ function response(
   if (msg === ".도움말") {
     replier.reply(
       "등록된 명령어\n[.그룹생성], [.인원추가 n],\n[.명단작성 이름 : git닉네임],\n[.인증]\n\n상세 명령어\n[.인원추가 2]\n[.명단추가 홍길동 : hgd123, 변사또 : bsd234]\n[.그룹삭제]" +
-        "\n명단 추가는 한번에 해주세요."
+        "\n명단 추가는 한번에 해주세요.\n[.기간체크 이름 달]"
     );
   } else if (msg.indexOf(".테스트") === 0) {
-    let test = Utils.parse("https://ghchart.rshah.org/INT31302").select(
-      "rect[data-date*=2019-" + msg.split(" ").reverse()[0] + "]"
-    );
-    let test2 = test.toString();
-    let checkcount = test2.match(/>/g).length;
-    var myArray = [];
-    var myArray2 = [];
-    var testcnt = 0;
-    for (let i = 0; i < checkcount; i++) {
-      myArray[i] = test2.substring(0, test2.indexOf(">") + 1);
-      test2 = test2.substring(test2.indexOf(">") + 1).trim();
-      let date = myArray[i].split(" ")[3];
-      let cnt = myArray[i].split(" ")[2];
-      if (cnt === 'data-score="0"') {
-        myArray2[testcnt] = date.substring(11, 21);
-        testcnt++;
-      }
-    }
-    myArray2.sort(function(a, b) {
-      let dateA = a.split("-")[2];
-      let dateB = b.split("-")[2];
-      return dateA > dateB ? 1 : -1;
-    });
-    var str = sender + "님이 커밋 안한 날짜";
-    for (let i = 0; i < testcnt; i++) {
-      str += "\n" + myArray2[i];
-    }
-    replier.reply(str);
+    const data = DataBase.getDataBase(room);
+    const context = data
+      .substring(data.indexOf(msg.split(" ")[1]))
+      .split("\n")[0];
+    const name = context.split(":")[0].trim();
+    const id = context.split(":")[1].trim();
+    replier.reply(name);
+    replier.reply(id);
     preChat = null;
   } else if (msg === ".그룹생성") {
     if (!isExist) {
@@ -206,6 +242,21 @@ function response(
     replier.reply("잠시만 기다려주세요!");
     let result = checkCommit(date, room, Doc, str, count);
     replier.reply(result.trim());
+    preChat = null;
+  } else if (msg.indexOf(".기간체크") === 0) {
+    replier.reply("잠시만 기다려주세요!");
+    let result = checkDate(msg, room);
+    switch (result) {
+      case "0":
+        replier.reply("올바른 달을 입력해주세요.");
+        break;
+      case "1":
+        replier.reply("등록되지 않은 이름입니다,");
+        break;
+      default:
+        replier.reply(result);
+        break;
+    }
     preChat = null;
   }
 }
